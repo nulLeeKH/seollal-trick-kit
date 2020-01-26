eead1e280c3afa4c87fdfa8fc80d033aa498b7c3
nulLeeKH <i_am@nulleekh.com>
2020-01-26 10:04:58 +0900

    [FIX] Fix wallpaper change function in seollal_trick_kit.sh

--- seollal_trick_kit.sh
+++ seollal_trick_kit.sh
@@ -1,2 +1,2 @@
-sqlite3 ~/Library/Application\ Support/Dock/desktoppicture.db "update data set value = '~/seollal-trick-kit/seollal_trick_kit_background.png'";
+osascript -e 'tell application "Finder" to set desktop picture to POSIX file "~/seollal-trick-kit/seollal_trick_kit_background.png"'
 killall Dock;
