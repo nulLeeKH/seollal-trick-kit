486cc712ed0e9b9371bae12e618d8fa210925f8c
nulLeeKH <i_am@nulleekh.com>
2020-01-26 09:49:52 +0900

    [FIX] Change path of wallpaper file in seollal_trick_kit.sh

--- seollal_trick_kit.sh
+++ seollal_trick_kit.sh
@@ -1,2 +1,2 @@
-sqlite3 ~/Library/Application\ Support/Dock/desktoppicture.db "update data set value = 'seollal_trick_kit_background.png'";
+sqlite3 ~/Library/Application\ Support/Dock/desktoppicture.db "update data set value = '~/seollal-trick-kit/seollal_trick_kit_background.png'";
 killall Dock;
